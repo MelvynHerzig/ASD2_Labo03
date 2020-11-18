46c0c7f1154b6eeaf6484154df559713db3b5d49
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-11-18 21:34:24 +0100

    Fini relisez et corrigez les pdf si nécessaire

--- L3d/ShortestPath.h
+++ L3d/ShortestPath.h
@@ -3,15 +3,15 @@
  * Author: Olivier Cuisenaire
  *
  * Created on 5. novembre 2014, 10:16
+ * Modified on 18. novembre 2020 by Berney Alec, Forestier Quentin, Herzig Melvyn
  */
 
 #ifndef ASD2_ShortestPath_h
 #define ASD2_ShortestPath_h
 
 #include <algorithm>
-#include <queue>
 #include <vector>
-#include <set>
+
 #include <functional>
 #include "EdgeWeightedDirected.h"
 
@@ -27,29 +27,32 @@ template<typename GraphType>   // Type du graphe pondere oriente a traiter
 // GraphType doit se comporter comme un
 // GraphWeightedDirected et definir le
 // type GraphType::Edge
-class ShortestPath {
+class ShortestPath
+{
 public:
 
-    typedef double Weight;
-    typedef EdgeWeightedDirected<Weight> Edge;
+   typedef double Weight;
+   typedef EdgeWeightedDirected<Weight> Edge;
 
-    // Listes d'arcs et de poids
-    typedef std::vector<Edge> Edges;
-    typedef std::vector<Weight> Weights;
+   // Listes d'arcs et de poids
+   typedef std::vector<Edge> Edges;
+   typedef std::vector<Weight> Weights;
 
-    // Renvoie la distance du chemin le plus court du sommet source a v
-    double distanceToVertex(int v) {
-        return distanceTo.at(v);
-    }
+   // Renvoie la distance du chemin le plus court du sommet source a v
+   double distanceToVertex (int v)
+   {
+      return distanceTo.at(v);
+   }
 
-    // Renvoie le dernier arc u->v du chemin le plus court du sommet source a v
-    Edge edgeToVertex(int v) {
-        return edgeTo.at(v);
-    }
+   // Renvoie le dernier arc u->v du chemin le plus court du sommet source a v
+   Edge edgeToVertex (int v)
+   {
+      return edgeTo.at(v);
+   }
 
 protected:
-    Edges edgeTo;
-    Weights distanceTo;
+   Edges edgeTo;
+   Weights distanceTo;
 };
 
 // Algorithme de BellmanFord.
@@ -61,88 +64,179 @@ template<typename GraphType> // Type du graphe pondere oriente a traiter
 // se comporter comme EdgeWeightedDirected, c-a-dire definir From(),
 // To() et Weight()
 
-class BellmanFordSP : public ShortestPath<GraphType> {
+class BellmanFordSP : public ShortestPath<GraphType>
+{
 
-    typedef ShortestPath<GraphType> BASE;
+   typedef ShortestPath<GraphType> BASE;
 protected:
 
-    typedef typename BASE::Edge Edge;
-    typedef typename BASE::Weight Weight;
+   typedef typename BASE::Edge Edge;
+   typedef typename BASE::Weight Weight;
 
-    // Relachement de l'arc e
-    void relax(const Edge& e) {
-        int v = e.From(), w = e.To();
-        Weight distThruE = this->distanceTo[v] + e.Weight();
+   // Relachement de l'arc e
+   void relax (const Edge &e)
+   {
+      int v = e.From(), w = e.To();
+      Weight distThruE = this->distanceTo[v] + e.Weight();
 
-        if (this->distanceTo[w] > distThruE) {
-            this->distanceTo[w] = distThruE;
-            this->edgeTo[w] = e;
-        }
-    }
+      if (this->distanceTo[w] > distThruE)
+      {
+         this->distanceTo[w] = distThruE;
+         this->edgeTo[w] = e;
+      }
+   }
 
 public:
 
-    // Constructeur a partir du graphe g et du sommet v a la source
-    // des plus courts chemins
-    BellmanFordSP(const GraphType& g, int v) {
+   // Constructeur a partir du graphe g et du sommet v a la source
+   // des plus courts chemins
+   BellmanFordSP (const GraphType &g, int v)
+   {
 
-        this->edgeTo.resize(g.V());
-        this->distanceTo.assign(g.V(), std::numeric_limits<Weight>::max());
+      this->edgeTo.resize(g.V());
+      this->distanceTo.assign(g.V(), std::numeric_limits<Weight>::max());
 
-        this->edgeTo[v] = Edge(v, v, 0);
-        this->distanceTo[v] = 0;
+      this->edgeTo[v] = Edge(v, v, 0);
+      this->distanceTo[v] = 0;
 
-        for (int i = 0; i < g.V() - 1; ++i)
-            g.forEachEdge([this](const Edge& e) {
-                this->relax(e);
-            });
-    }
+      for (int i = 0; i < g.V() - 1; ++i)
+      {
+         g.forEachEdge([this] (const Edge &e)
+                       {
+                          this->relax(e);
+                       });
+      }
+   }
 };
 
-template<typename GraphType> // Type du graphe pondere oriente a traiter
-// GraphType doit se comporter comme un
-// GraphWeightedDirected et definir forEachEdge(Func),
-// ainsi que le type GraphType::Edge. Ce dernier doit
-// se comporter comme EdgeWeightedDirected, c-a-dire definir From(),
-// To() et Weight()
-class BellmanFordCycleDetection : public BellmanFordSP<GraphType> {
+/**
+ * @brief Vérifie si le graphe contient un circuit à coût négatif.
+ * @tparam GraphType Type du graphe pondere oriente à traiter.
+ * @details GraphType doit se comporter comme un
+ *          GraphWeightedDirected et definir forEachEdge(Func),
+ *          ainsi que le type GraphType::Edge. Ce dernier doit
+ *          se comporter comme EdgeWeightedDirected, c-a-dire definir From(),
+ *          To() et Weight().
+ */
+template<typename GraphType>
+class BellmanFordCycleDetection : public BellmanFordSP<GraphType>
+{
 
 private:
-    typedef BellmanFordSP<GraphType> BASE;
-    typedef typename BASE::Edge Edge;
-    typedef typename BASE::Weight Weight;
+   typedef BellmanFordSP<GraphType> BASE;
+   typedef typename BASE::Edge Edge;
+   typedef typename BASE::Weight Weight;
+
+   std::vector<bool> isVerified;
+   int source;
+
+   /**
+    * @brief vérifie si le sommet i peut remonter jusqu'a la source.
+    * @param i No du sommet à tester.
+    * @return Retour le numéro de la source sinon le numéro du premier sommet
+    *         rencontré appartenant à la boucle.
+    */
+   int checkAccessToSource (int i)
+   {
+      std::vector<bool> marked;
+      marked.resize(isVerified.size());
+      std::fill_n(marked.begin(), marked.size(), false);
+
+      Edge e = BASE::edgeTo[i];
+
+      // Temps qu'on ne recontre pas un sommet déjà traité.
+      while (!isVerified[e.From()])
+      {
+         isVerified[e.From()] = true;
+         marked[e.From()] = true;
+         e = BASE::edgeTo[e.From()];
+      }
+
+      // Si marked, une boucle est détectée, sinon on a atteint
+      // une branche de l'arbre qui à accès à la source
+      return marked[e.From()] ? e.From() : source;
+   }
+
+   /**
+    * @brief Calcule les arcs formant une boucle. i est un sommet de la boucle.
+    * @param i Sommet issu de la boucle.
+    * @return Liste des arcs formant la boucle.
+    * @details Si i n'appartient pas à une boucle, retourne une liste vide.
+    */
+   std::list<Edge> getLoop (int i)
+   {
+      std::list<Edge> loop;
+      Edge e = BASE::edgeTo[i];
+
+      while (e.From() != i)
+      {
+         loop.push_back(e);
+         e = BASE::edgeTo[e.From()];
+      }
+
+      loop.push_back(e);
+
+      std::reverse(loop.begin(), loop.end());
+
+      return e.From() == source ? std::list<Edge>() : loop;
+   }
 
 public:
 
-    // Constructeur a partir du graphe g et du sommet v a la source
-    // des plus courts chemins
-    BellmanFordCycleDetection(const GraphType& g, int v) : BASE(g, v) {
-/****
-*
-*  A IMPLEMENTER
-*  Doit lancer une GraphNegativeDirectedCycleException si un circuit absorbant est detecté
-*
-****/
-    }
+   /**
+    * @brief Constructeur a partir du graphe g et du sommet v a la source
+    *        des plus courts chemins
+    * @param g Graphe où calculer les plus courts chemins.
+    * @param v Numéro du sommet source.
+    * @Throws Lance une GraphNegativeDirectedCycleException en cas de circuit à
+    *         coût négatif.
+    */
+   BellmanFordCycleDetection (const GraphType &g, int v) : BASE(g, v), source(v)
+   {
+      isVerified.resize(g.V());
+      std::fill_n(isVerified.begin(), isVerified.size(), false);
+      isVerified[source] = true;
 
-    //exception si le graphe contient un circuit absorbant
-    class GraphNegativeDirectedCycleException : public std::exception {
+      // Vérifie si chaque sommet peut atteindre la source
+      g.forEachVertex([&] (int s)
+                      {
+                         if (!isVerified[s])
+                         {
+                            int vReached = checkAccessToSource(s);
+                            if (vReached != source)
+                            {
+                               throw GraphNegativeDirectedCycleException(getLoop(vReached));
+                            }
+                         }
+                      });
 
-    private:
-        //liste des arcs du circuit absorbant
-        const std::list<Edge> directedCycle;
+   }
 
-    public:
-        GraphNegativeDirectedCycleException(const std::list<Edge> directedCycle) noexcept: exception(), directedCycle(directedCycle) {}
+   /**
+    * @brief Exception si le graphe contient un circuit absorbant
+    */
+   class GraphNegativeDirectedCycleException : public std::exception
+   {
 
-        virtual const char* what() const noexcept {
-            return "Shortest Path impossible : the graph contains a negative directed cycle";
-        }
+   private:
+      //liste des arcs du circuit absorbant
+      const std::list<Edge> directedCycle;
 
-        const std::list<Edge>& Cycle() const noexcept {
-            return directedCycle;
-        }
-    };
+   public:
+      GraphNegativeDirectedCycleException (const std::list<Edge> directedCycle) noexcept
+              : exception(), directedCycle(directedCycle)
+      {}
+
+      virtual const char *what () const noexcept
+      {
+         return "Shortest Path impossible : the graph contains a negative directed cycle";
+      }
+
+      const std::list<Edge> &Cycle () const noexcept
+      {
+         return directedCycle;
+      }
+   };
 };
 
 #endif
--- L3d/main.cpp
+++ L3d/main.cpp
@@ -3,6 +3,7 @@
  * Author: Antoine Rochat
  *
  * Created on 14. octobre 2020
+ * Modified on 18. novembre 2020 by Berney Alec, Forestier Quentin, Herzig Melvyn
  */
 
 #include <iostream>
@@ -13,27 +14,73 @@
 #include "GraphWeightedDirected.h"
 
 using namespace std;
+using costFunction = const std::function<double (double)>;
+
+/*
+ * @brief Affiche la transformation de 1 par chaque poids des arcs de loop issu d'un
+ *        graphe à symboles
+ * @details Edge doit implémenter int From() const, int To() const, double Weight() const
+ *          SymbolGraphe doit implémenter std::string symbol(int idx) const.
+ */
+template<typename Edge, typename SymbolGraphe>
+void afficherCircuit (const std::list<Edge> &loop, const SymbolGraphe &sg)
+{
+   double result = 1;
+   double convertedWeight;
+
+   for (const Edge &e : loop)
+   {
+      convertedWeight = exp(-e.Weight());
+      cout << result << " " << sg.symbol(e.From()) << " (x" << convertedWeight << ")" << " -> ";
+      result *= convertedWeight;
+   }
+
+   cout << result << " " << sg.symbol(loop.back().To()) << endl;
+}
 
 //ARGS reseau.txt
 //ARGS reseau2.txt
-int main(int argc, const char* argv[]) {
+int main (int argc, const char *argv[])
+{
 
-    if (argc < 2) {
-        cerr << "Aucun fichier fourni en argument" << endl;
-        return EXIT_FAILURE;
-    }
+   if (argc < 2)
+   {
+      cerr << "Aucun fichier fourni en argument" << endl;
+      return EXIT_FAILURE;
+   }
 
-    typedef GraphWeightedDirected<double> Graph;
+   typedef GraphWeightedDirected<double> Graph;
 
-    for (int i = 1; i < argc; ++i) {
-        cout << "Fichier " << argv[i] << " :" << endl;
-/****
-*
-*  A IMPLEMENTER
-*
-****/
-    }
+   costFunction getTaux = [&] (double d)
+   {
+      // Avec cette transformation, on ramnène les multiplication du problème
+      // d'arbitrage, à des addition.
+      // On inverse les nombre ainsi obtenus, pour que les conversion qui permettent
+      // des gains ( > 1) soient négatives et ainsi détectables par BellmanFord.
+      return -log(d);
+   };
 
-    return EXIT_SUCCESS;
+   for (int i = 1; i < argc; ++i)
+   {
+      cout << "Fichier " << argv[i] << " :" << endl;
+      SymbolGraphWeightedDirected<Graph> symbolGraph(argv[i], getTaux, ';');
+
+      try
+      {
+         BellmanFordCycleDetection<Graph> bfcd(symbolGraph.G(), 0);
+      }
+      catch (const BellmanFordCycleDetection<Graph>::GraphNegativeDirectedCycleException& e)
+      {
+
+         cout << "Circuit absorbant détecté :" << endl;
+
+         afficherCircuit(e.Cycle(), symbolGraph);
+
+         return EXIT_FAILURE;
+      }
+
+      cout << "Aucun circuit absorbant détecté" << endl;
+   }
+
+   return EXIT_SUCCESS;
 }
-
